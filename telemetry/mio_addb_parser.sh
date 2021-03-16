292e3519320b4e5ff29f9fef8b5cc4f20538557c
Sining Wu <sining.wu@seagate.com>
2021-03-16 02:47:04 +0000

    Add sample telemetry analysis utilities.
    - Demostrate a workflow of parsing telemetry records, extracting useful
      information and storing into a databse.
    
    - A few utilities to show how to query telemetry database and analyse
      telemetry data: MIO operation time histogram and zoom-in(break-down)
      analysis of an MIO operation execution time.
    
    - Those utilities are Modified from cortx-motr's chronometry. The utilities
      only work on ADDB records and support sqlite3 database currently, but
      they can be easily adpat to other kinds of telemetry backends and databases.
    
    Signed-off-by: Sining Wu <sining.wu@seagate.com>

---
+++ telemetry/mio_addb_parser.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+dir=$( cd "$(dirname "$0")" ; pwd -P )
+
+addb=$1
+if [ ! -f $addb ]; then
+	echo "Can't find ADDB dump file!"
+	echo "Usage: $ mio_addb_parser addb_dump_file"
+	return 1
+fi
+
+# Filter out those MIO records.
+tmp=.tmp.txt
+mio_addb_id=30000
+echo "Filter MIO records ..."
+grep -E $mio_addb_id $addb > $tmp
+
+# Parse MIO records.
+parser=$dir/mio_telemetry_parser
+if [ ! -f $parser ]; then
+	echo "Can't find MIO telemetry parser!"
+	return 1
+fi
+eval "$parser $tmp >> $addb"
+if [ $? -ne 0 ]; then
+	echo "Failed to parse MIO telemetry records!"
+	return 1
+fi
